31ff3eca84082b05caaba5ffaf6ecb392cafce82
quhairfoir <moragstc@gmail.com>
2018-08-14 16:44:56 -0400

    .tweet hover states finished, app.js functions started

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,5 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- 
-
+$(document).ready(function() {
+  
+  const createTweetElement = function (tweetObj) {
+  let $tweet = $("<article>").addClass("tweet");
+    $tweet.append("<header>");
+  return $tweet;
+  };
+});
