9d4cf14358ffb267df72d44c9a49dd640bc18a05
quhairfoir <moragstc@gmail.com>
2018-08-15 11:25:48 -0400

    tweets fetched with AJAX /tweets route

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+ // function to convert js miliseconds to readable date
 function timeSince(date) {
   var seconds = Math.floor((new Date() - date) / 1000);
   var interval = Math.floor(seconds / 31536000);
@@ -29,82 +30,48 @@ function timeSince(date) {
   return Math.floor(seconds) + " seconds";
 };
 
-
+//jQuery functions and code
 $(document).ready(function() {
 
+  // creates properly formatted html object for tweet
+  function createTweetElement (tweetObj) {
+    let $tweet = $("<article>").addClass("tweet");
+    let $header = $("<header>");
+    $($header).append("<h2>" + tweetObj.user.name + "</h2>");
+    $($header).append('<img src="'+ tweetObj.user.avatars.small + '">');
+    $($header).append("<p>" + tweetObj.user.handle + "</p>");
+    $tweet.append($header);
+    $tweet.append("<p>" + tweetObj.content.text + "</p>");
+    $tweet.append('<footer>' + timeSince(tweetObj.created_at) + ' ago<i class="fas fa-heart"></i><i class="fas fa-retweet"></i><i class="fas fa-flag"></i></footer>');
+    return $tweet;
+  };
+
+  // appends tweets to DOM
   function renderTweets(tweets) {
+    console.log(tweets);
     for (let tweet of tweets) {
       let newTweet = createTweetElement(tweet);
       $('#tweetholder').append(newTweet);
     }
   };
   
-  function createTweetElement (tweetObj) {
-  let $tweet = $("<article>").addClass("tweet");
-  let $header = $("<header>");
-  $($header).append("<h2>" + tweetObj.user.name + "</h2>");
-  $($header).append('<img src="'+ tweetObj.user.avatars.small + '">');
-  $($header).append("<p>" + tweetObj.user.handle + "</p>");
+  //fetches tweets from /tweets page (JSON object)
+  function loadTweets() {
+    $.ajax({
+      method: "GET",
+      url: "/tweets"
+    })
+    .then(function (something){
+      renderTweets(something);
+    });
+  }; 
   
-  $tweet.append($header);
-  $tweet.append("<p>" + tweetObj.content.text + "</p>");
-  $tweet.append('<footer>' + timeSince(tweetObj.created_at) + ' ago<i class="fas fa-heart"></i><i class="fas fa-retweet"></i><i class="fas fa-flag"></i></footer>');
-  return $tweet;
-  };
+  loadTweets();
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
-
-  renderTweets(data);
-
-
-  $("form > input").click(function (event){
+  // AJAX "POST" request 
+  $("form").on("submit", (event) => {
     event.preventDefault();
-    $.post("/tweets", $('form').serialize());
+    $.post("/tweets", $("form").serialize());
   });
 
 });
