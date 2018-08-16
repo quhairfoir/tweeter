d08bfc2c940411e3205879d927abbf253d9716a7
Morag St. Clair <moragstc@gmail.com>
2018-08-16 20:15:26 +0000

    mongo fully linked, site working as before

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,8 +1,5 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
-
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
@@ -15,14 +12,30 @@ module.exports = function makeDataHelpers(db) {
       });
     },
 
+    // getTweets: function(callback) {
+    //   db.collection('tweets').find().toArray((err, tweets) => {
+    //     if (err) {
+    //       return callback(err)
+    //     }
+    //     callback(null, tweets);
+    //   });
+    // }
+
+    // Get all tweets in `db`, sorted by newest first
+    // getTweets: function(callback) {
+    //   simulateDelay(() => {
+    //     const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+    //     callback(null, db.tweets.sort(sortNewestFirst));
+    //   });
+    // }
+
     getTweets: function(callback) {
-      db.collection('tweets').find().toArray((err, tweets) => {
+      db.collection('tweets').find().sort({ created_at: -1 }).toArray((err, tweets) => {
         if (err) {
           return callback(err)
         }
         callback(null, tweets);
-      });
-    }
-
-  };
+      }
+      )}
+  }
 }
