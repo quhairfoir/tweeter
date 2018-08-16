00f9c31558c2b30311a54509d98dce10fa8f8627
Morag St. Clair <moragstc@gmail.com>
2018-08-16 19:26:46 +0000

    mongo connection functional

--- server/index.js
+++ server/index.js
@@ -10,20 +10,24 @@ const app           = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
-const db = require("./lib/db-connection.js");
 
-db.initDb();
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-const DataHelpers = require("./lib/data-helpers.js")(db);
+  const DataHelpers = require("./lib/data-helpers.js")(db);
+  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+  app.use("/tweets", tweetsRoutes);
 
-// Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
+})
 
-app.listen(PORT, () => {
+  app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -3,33 +3,26 @@
 // Simulates the kind of delay we see with network or filesystem operations
 const simulateDelay = require("./util/simulate-delay");
 
-
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+      db.collection('tweets').insertOne(newTweet, (err, results) => {
+        if (err) throw err;
         callback(null, true);
       });
     },
 
-   // Get all tweets in `db`, sorted by newest first
-   getTweets: function(callback) {
-     debugger;
-    simulateDelay(() => {
-      const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-      callback(null, db.getDb().collection("tweets").find().toArray(sortNewestFirst));
-    });
-  }
-
-    // getTweets: function(callback) {
-    //   const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-    //   db.collection("tweets").find().toArray(callback);
-    //   callback(null, db.tweets.sort(sortNewestFirst));
-    // }
+    getTweets: function(callback) {
+      db.collection('tweets').find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err)
+        }
+        callback(null, tweets);
+      });
+    }
 
   };
 }
